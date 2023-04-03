206aff846cb807f45d6824cc471ab67bde408191
littlemary1379@gamil.com <littlemary1379@gmail.com>
2023-04-03 17:47:52 +0900

    backjun 230403

---
+++ backjun_230403/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    question10869()
+}
+
+fun question10869() {
+    val sc: Scanner = Scanner(System.`in`)
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+
+}
