ce9478c5e449cb7cc24edd2a7114988ba197ac8f
littlemary1379@gamil.com <littlemary1379@gmail.com>
2023-04-04 17:36:31 +0900

    backjun 230404_5

---
+++ backjun_230404_5/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    question10171()
+}
+
+fun question10171() {
+    println("\\    /\\")
+    println(" )  ( ')")
+    println("(  /  )")
+    println(" \\(__)|")
+}
