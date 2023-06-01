7db68814d78272ee18c66d4cc8ea2ebafe38f08f
littlemary1379@gamil.com <littlemary1379@gmail.com>
2023-06-01 11:18:54 +0900

    backjun 230601

--- backjun/backjun_230601/src/main/kotlin/Main.kt
+++ backjun/backjun_230601/src/main/kotlin/Main.kt
@@ -8,12 +8,12 @@ fun question5622() {
 
     word.forEach {
         var code = it.code - 65
-        if(code < 18) {
-            time += (code) / 3 + 2 + 1
+        time += if(code < 18) {
+            (code) / 3 + 2 + 1
         } else if(code < 25) {
-            time += (code - 1) / 3 + 2 + 1
+            (code - 1) / 3 + 2 + 1
         } else {
-            time += (code - 2) / 3 + 2 + 1
+            (code - 2) / 3 + 2 + 1
         }
     }
 
---
+++ backjun/backjun_230601_2/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    question11718()
+}
+
+fun question11718() {
+    while (true) {
+        var line = readLine()
+        if(line.isNullOrEmpty()) {
+            break
+        }
+        println(line)
+    }
+}
