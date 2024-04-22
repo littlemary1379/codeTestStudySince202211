b32eb8c9da1b0797bd61127f530665cd68bc8543
mary <littlemary1379@gmail.com>
2024-04-22 16:54:18 +0900

    beakjoon_240422

--- backjun/beakjoon_240422/src/main/kotlin/Main.kt
+++ backjun/beakjoon_240422/src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.ceil
+import kotlin.system.measureTimeMillis
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    question17427()
+}
+
+fun question17427() {
+
+    val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
+    val bufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
+
+    val number = bufferedReader.readLine().toInt()
+    var sum = 0L
+    for(i in 1 .. number) {
+        sum += (number / i) * i
+    }
+
+    println(sum)
+
 }
