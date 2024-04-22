9d77b2e2d04bc8ecaeed4256be7b54037da3b7e7
mary <littlemary1379@gmail.com>
2024-04-22 15:27:40 +0900

    beakjoon_240419_4 수정

--- backjun/beakjoon_240419_4/src/main/kotlin/Main.kt
+++ backjun/beakjoon_240419_4/src/main/kotlin/Main.kt
@@ -2,18 +2,10 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.lang.Exception
-import java.text.DecimalFormat
 import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.pow
-import kotlin.math.sqrt
-import kotlin.system.measureTimeMillis
 
 
 fun main(args: Array<String>) {
-//    val measure = measureTimeMillis { question17425() }
-//    println(measure)
     question17425()
 }
 
@@ -34,13 +26,12 @@ fun question17425() {
     bufferedWriter.flush()
 }
 
-// true : 소수, false : 합성수, 0, 1
 fun eratosPrime(size : Int) : Array<Long> {
 
     val mesArray = Array(size + 1) { 1L }
     val mesAddArray = Array(size + 1) { 1L }
 
-    for(i in 2 ..< mesArray.size) {
+    for(i in 2 ..< mesArray.size/ 2) {
         for(j in 1..< ceil(mesAddArray.size.toDouble() / i).toInt()) {
             mesArray[i * j] += i.toLong()
         }
