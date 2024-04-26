b074b4ea9f94bbc2c8e2e40c56d8b316a9eaabb8
mary <littlemary1379@gmail.com>
2024-04-26 10:09:57 +0900

    beakjoon_question5337, 5338

---
+++ backjun/beakjoon_question5337/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    question5337()
+}
+
+fun question5337() {
+    println(".  .   .")
+    println("|  | _ | _. _ ._ _  _")
+    println("|/\\|(/.|(_.(_)[ | )(/.")
+}
---
+++ backjun/beakjoon_question5338/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    question5338()
+}
+
+fun question5338() {
+    println("       _.-;;-._")
+    println("'-..-'|   ||   |")
+    println("'-..-'|_.-;;-._|")
+    println("'-..-'|   ||   |")
+    println("'-..-'|_.-''-._|")
+}
