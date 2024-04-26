c6f81c83494de62c1de214d8b2888897848ee5c2
mary <littlemary1379@gmail.com>
2024-04-26 11:17:05 +0900

    beakjoon_question5339

---
+++ backjun/beakjoon_question5339/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    question5339()
+}
+
+fun question5339() {
+    println("     /~\\")
+    println("    ( oo|")
+    println("    _\\=/_")
+    println("   /  _  \\")
+    println("  //|/.\\|\\\\")
+    println(" ||  \\ /  ||")
+    println("============")
+    println("|          |")
+    println("|          |")
+    println("|          |")
+}
