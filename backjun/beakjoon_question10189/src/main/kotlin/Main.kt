3f019a83308e7918d0119b6d92d0a30ca660c8aa
mary <littlemary1379@gmail.com>
2024-04-29 10:21:32 +0900

    beakjoon_question10170, 10189

---
+++ backjun/beakjoon_question10189/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    question10189()
+}
+
+fun question10189() {
+    println("#  # #### #### #  #")
+    println("#### #  # #  # # #")
+    println("#### #  # #  # # #")
+    println("#  # #### #### #  #")
+}
