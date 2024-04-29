bd9d13ad1a851c5cad5f04597940b8dc34915655
mary <littlemary1379@gmail.com>
2024-04-29 10:09:11 +0900

    beakjoon_question9654

---
+++ backjun/beakjoon_question9654/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    question9654()
+}
+
+fun question9654() {
+    println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+    println("N2 Bomber      Heavy Fighter  Limited    21        ")
+    println("J-Type 327     Light Combat   Unlimited  1         ")
+    println("NX Cruiser     Medium Fighter Limited    18        ")
+    println("N1 Starfighter Medium Fighter Unlimited  25        ")
+    println("Royal Cruiser  Light Combat   Limited    4         ")
+}
