586e98053222d193b80e91bca4d64daaf279af89
mary <littlemary1379@gmail.com>
2024-04-30 09:57:27 +0900

    beakjoon_question23235

---
+++ backjun/beakjoon_question23235/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    question23235()
+}
+
+fun question23235() {
+    var caseCount = 1
+    while (true) {
+        val read = readln()
+        if(read == "0") {
+            break
+        } else {
+            println("Case $caseCount: Sorting... done!")
+            caseCount++
+        }
+    }
+}
