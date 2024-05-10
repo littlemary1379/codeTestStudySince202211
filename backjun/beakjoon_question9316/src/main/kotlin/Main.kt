2e4f58fcdd191d6712b488329ee36eef95be129f
littlemary1379@gamil.com <littlemary1379@gmail.com>
2024-05-10 18:30:50 +0900

    beakjoon_question9316

---
+++ backjun/beakjoon_question9316/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    question9316()
+}
+
+fun question9316() {
+    val count = readln().toInt()
+    for (i in 1 .. count) {
+        println("Hello World, Judge $i!")
+    }
+}
