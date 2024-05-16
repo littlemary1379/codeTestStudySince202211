191d34979737139fac982b7e968b49e25f723387
mary <littlemary1379@gmail.com>
2024-05-16 10:02:52 +0900

    beakjoon_question13752

---
+++ backjun/beakjoon_question13752/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    question13752()
+}
+
+fun question13752() {
+    val count = readln().toInt()
+    for (i in 1 .. count) {
+        println("=".repeat(readln().toInt()))
+    }
+}
