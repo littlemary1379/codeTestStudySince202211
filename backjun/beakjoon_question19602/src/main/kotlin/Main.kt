8ebead72390f11730facef29bf92edbd06a43a38
mary <littlemary1379@gmail.com>
2024-05-21 15:04:43 +0900

    beakjoon_question19602

---
+++ backjun/beakjoon_question19602/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    question19602()
+}
+
+fun question19602() {
+    if(readln().toInt() * 1 + readln().toInt() * 2 + readln().toInt() * 3 >= 10) {
+        println("happy")
+    } else {
+        println("sad")
+    }
+}
